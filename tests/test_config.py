a164eb04cdf4746d8d91962ef39009f66d65593c
Kaspar Peterson <kasparpeterson@hotmail.com>
2024-09-05 12:19:40 +0300

    Fixes unit tests.

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,4 +2,4 @@ from galadriel_node.config import config
 
 
 def test_config():
-    assert config.GALADRIEL_RPC_URL == "ws://localhost:5000/v1/node"
+    assert config.GALADRIEL_RPC_URL == "wss://api.galadriel.com/v1/node"

c93879ecfd776de501c423dea66f676ea03f8084
Mohamed Zahoor <940575+jmozah@users.noreply.github.com>
2024-10-07 14:39:15 +0530

    Trimed other work and adding only lint to pre-commit for now

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -6,11 +6,6 @@ cp scripts/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 echo "pre-commit hook installed"
 
-# Install pommit-msg hook
-cp scripts/commit-msg .git/hooks/commit-msg
-chmod +x .git/hooks/commit-msg
-echo "commit-msg hook installed"
-
 # Install pre-push hook
 cp scripts/pre-push .git/hooks/pre-push
 chmod +x .git/hooks/pre-push

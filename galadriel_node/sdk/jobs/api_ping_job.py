1a50b319c299b55b3267fb1ced69b47861c8ff0b
Han <4738254+h4n0@users.noreply.github.com>
2024-11-08 14:02:55 +0100

    Refactored the api ping job

--- galadriel_node/sdk/jobs/api_ping_job.py
+++ galadriel_node/sdk/jobs/api_ping_job.py
@@ -17,21 +17,18 @@ class ApiPingJob:
             try:
                 await asyncio.sleep(config.GALADRIEL_API_PING_INTERVAL)
                 ping_time = self._check_api_ping_time()
-                if ping_time is not None:
-                    await self._append_ping_time(ping_time)
-                    rich.print(f"API ping time: {ping_time}ms")
-                else:
-                    await self._append_ping_time(ping_time)
-                    rich.print("Failed to ping the Galadriel API.")
-            except Exception as e:
                 await self._append_ping_time(ping_time)
+            except Exception as e:
+                await self._append_ping_time(None)
                 rich.print(f"Error occurs in API ping job: {e}")
 
     def _check_api_ping_time(self) -> Optional[int]:
         ping_time = ping(self.domain, unit="ms")
-        rich.print(f"ping to domain: {self.domain}")
-        if ping_time is None or ping_time is False:
+        rich.print(f"Ping to domain: {self.domain}")
+        if not ping_time:
+            rich.print("Failed to ping the Galadriel API.")
             return None
+        rich.print(f"API ping time: {ping_time}ms")
         return int(ping_time)
 
     async def _append_ping_time(self, ping_time: Optional[int]):

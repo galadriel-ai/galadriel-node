38064ebdd1f31c3f3490c20b0f0bc34bff964264
Maciej Witowski <maciej.witowski@protocol.ai>
2024-12-03 14:12:10 +0100

    WIP

--- galadriel_node/sdk/logging_utils.py
+++ galadriel_node/sdk/logging_utils.py
@@ -20,6 +20,8 @@ def init_logging(debug: bool):
     formatter = logging.Formatter("%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
     rich_handler.setFormatter(formatter)
     logger.addHandler(rich_handler)
+    if len(logger.handlers) > 1:
+        logger.removeHandler(logger.handlers[1])
 
 
 def get_node_logger():

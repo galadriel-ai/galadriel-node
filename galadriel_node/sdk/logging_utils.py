95a34ffa7f75dca62f807a7a54b992e88c94d7c2
Maciej Witowski <maciej.witowski@protocol.ai>
2024-12-03 14:37:54 +0100

    b

--- galadriel_node/sdk/logging_utils.py
+++ galadriel_node/sdk/logging_utils.py
@@ -8,10 +8,6 @@ GALADRIEL_NODE_LOGGER = "galadriel_node"
 
 def init_logging(debug: bool):
     log_level = DEBUG if debug else INFO
-    root_logger = logging.getLogger()
-    for handler in root_logger.handlers[:]:
-        root_logger.removeHandler(handler)
-
     rich_handler = RichHandler(
         rich_tracebacks=True,
         show_time=True,
@@ -24,8 +20,7 @@ def init_logging(debug: bool):
     formatter = logging.Formatter("%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
     rich_handler.setFormatter(formatter)
     logger.addHandler(rich_handler)
-    # logger.propagate = False
-
+    logger.propagate = False
 
 def get_node_logger():
     return logging.getLogger(GALADRIEL_NODE_LOGGER)

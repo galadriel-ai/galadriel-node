71b8fe90c75fa79c2e75b69fda1276ea69c7bb99
internet-zero <75987671+internet-zero@users.noreply.github.com>
2024-12-10 09:44:16 +0530

    Update network.py

--- galadriel_node/cli/network.py
+++ galadriel_node/cli/network.py
@@ -3,6 +3,10 @@ from http import HTTPStatus
 
 import typer
 
+from rich.console import Console
+from rich.table import Table
+from rich import print as rprint
+
 from galadriel_node.config import config
 from galadriel_node.sdk.upgrade import version_aware_get
 
@@ -12,6 +16,7 @@ network_app = typer.Typer(
     no_args_is_help=True,
 )
 
+console = Console()
 
 @network_app.command("stats", help="Get current network stats")
 def network_stats(
@@ -23,19 +28,23 @@ def network_stats(
     status, response_json = asyncio.run(
         version_aware_get(api_url, "network/stats", api_key)
     )
+    
     if status == HTTPStatus.OK and response_json:
         print_network_status(response_json)
     else:
-        print("Failed to get node status..", flush=True)
-
+        rprint("[bold red]Failed to get node status..[/bold red]")
 
 def print_network_status(data):
-    print(f"nodes_count: {data['nodes_count']}")
-    print(f"connected_nodes_count: {data['connected_nodes_count']}")
-    print(f"network_throughput: {data['network_throughput']}")
-    print("throughput by model:")
+    console.print(f"[bold]nodes_count:[/bold] {data['nodes_count']}")
+    console.print(f"[bold]connected_nodes_count:[/bold] {data['connected_nodes_count']}")
+    console.print(f"[bold]network_throughput:[/bold] {data['network_throughput']}")
+
+    console.print("[bold]throughput by model:[/bold]")
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("Model Name", style="dim", width=20)
+    table.add_column("Throughput", justify="right", style="dim", width=15)
 
     for model in data["network_models_stats"]:
-        print(f"    model_name: {model['model_name']}")
-        print(f"    throughput: {model['throughput']}")
-        print()
+        table.add_row(model["model_name"], str(model["throughput"]))
+
+    console.print(table)
